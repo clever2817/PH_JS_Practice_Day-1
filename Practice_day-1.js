a512b3bf1454eb90603f87141ac1488b546aace3
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-28 00:55:37 +0600

    Problem 4 Solved.

--- Practice_day-1.js
+++ Practice_day-1.js
@@ -29,3 +29,19 @@ let num = 119;
 let remainderOfTheNumber = 119 % 5;
 
 console.log(remainderOfTheNumber);
+
+
+// Problem 4
+
+const a = isNaN('11');
+
+console.log(a);
+
+// In the above code, a is a variable which value can not be changed. After that, the output of the value a is false. Here the isNaN() function checks whether the value in it is a number or not, and it returns true if the value is not a number. On the other hand it returns false when the value is a number. In the above code we can see the value in isNaN() function is a string but the output is false, because the isNaN() function converts the value in a number then checks.
+
+
+const b = isNaN(2 - 10);
+
+console.log(b);
+
+// In the above code, the value in the isNaN() function is actually a number which is (2 - 10) = -8. So, it returns false as it should be.
