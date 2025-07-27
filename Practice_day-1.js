f2a56b1dc63e2c91d2b58d383dd933d34046b3f5
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-27 21:11:10 +0600

    Problem 1 and 2 solved

---
+++ Practice_day-1.js
@@ -0,0 +1,22 @@
+// Problem 1
+
+let totalMoney = 1000;
+
+let totalCost = 700;
+
+let remainingMoney = totalMoney - totalCost;
+
+console.log(remainingMoney);
+
+
+// Problem 2
+
+let mathMarks = 75.25;
+let biologyMarks = 65;
+let chemistryMarks = 80;
+let physicsMarks = 35.45;
+let banglaMarks = 99.50;
+
+let avgMarks = (mathMarks + biologyMarks + chemistryMarks+ physicsMarks + banglaMarks) / 5;
+
+console.log(avgMarks.toFixed(2));
